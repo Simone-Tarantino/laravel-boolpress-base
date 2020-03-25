f7afee3964d5f9b1d7dc0c376a2ec4d53c6c4c0b
Simone Tarantino <tarantinosimonew@gmail.com>
2020-03-25 15:12:51 +0100

    Generazione fake seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -11,8 +11,15 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 5; $i++) { 
+            $newPost = New Post;
+            $newPost->title = $faker->realText($maxNbChars = 50, $indexSize = 2);
+            $newPost->author = $faker->name;
+            $newPost->content = $faker->realText($maxNbChars = 350, $indexSize = 2);
+            $newPost->localization = $faker->state;
+            $newPost->save();
+        }
     }
 }

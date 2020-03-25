282507be0dee2f193166373c730a66c55e125a86
Simone Tarantino <tarantinosimonew@gmail.com>
2020-03-25 15:39:28 +0100

    Index completa

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.layout')
+@section('main')
+<h1>Tutti i posts</h1>
+    @foreach ($posts as $post)
+    <div class="single-post">
+        <ul>
+            <li>
+                <h2>{{$post->title}}</h2>
+            </li>
+            <li>
+                <h3>{{$post->author}}</h3>
+            </li>
+            <li>
+                <h4>{{$post->localization}}</h4>
+            </li>
+            <li>
+                <div class="content">
+                    <p>{{$post->content}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <hr>
+    @endforeach
+@endsection
